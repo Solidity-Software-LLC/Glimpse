7eb463b8d7a5ed5ed82e92365ab59b0da77902d2
Jonathan Mis <jonathan_mis@soliditysoftware.com>
2023-12-07 16:18:16 -0500

    Remove redundant feature reducers.  Remove screenshots for removed windows.

--- src/Glimpse/State/Reducers.cs
+++ src/Glimpse/State/Reducers.cs
@@ -30,11 +30,8 @@ public class AllReducers
 			.On<RemoveWindowAction>((s, a) => s.Remove(a.WindowProperties))
 			.On<UpdateWindowAction>((s, a) => s.UpsertOne(a.WindowProperties))
 			.On<AddWindowAction>((s, a) => s.UpsertOne(a.WindowProperties)),
-		FeatureReducer.Build(new DataTable<ulong, WindowProperties>())
-			.On<RemoveWindowAction>((s, a) => s.Remove(a.WindowProperties))
-			.On<UpdateWindowAction>((s, a) => s.UpsertOne(a.WindowProperties))
-			.On<AddWindowAction>((s, a) => s.UpsertOne(a.WindowProperties)),
 		FeatureReducer.Build(new DataTable<ulong, Pixbuf>())
+			.On<RemoveWindowAction>((s, a) => s.Remove(a.WindowProperties.WindowRef.Id))
 			.On<UpdateScreenshotsAction>((s, a) => s.UpsertMany(a.Screenshots)),
 		FeatureReducer.Build(new DataTable<string, Pixbuf>())
 			.On<AddOrUpdateNamedIconsAction>((s, a) => s.UpsertMany(a.Icons)),
